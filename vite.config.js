54fdc4df23ffa7e174d18124c30f5fef08c68d10
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2024-06-06 22:08:40 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 export default defineConfig({
+    base: '/',
     plugins: [react(), eslint()],
 })
