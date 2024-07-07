c1532b88fe36899ba9dfed959cab0227a9e128d8
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2024-07-07 22:01:30 +0800

    add menu animate

--- src/ui/MobileNav.jsx
+++ src/ui/MobileNav.jsx
@@ -18,10 +18,20 @@ function MobileNav() {
     return (
         <nav className="lg:hidden">
             <div
-                onClick={() => setOpenMenu(true)}
-                className="absolute right-10 top-10 h-6 w-6 cursor-pointer"
+                // onClick={() => setOpenMenu(true)}
+                // className="absolute right-10 top-10 h-6 w-6 cursor-pointer"
+                className="absolute right-10 top-10 z-30 flex cursor-pointer flex-col gap-[4.5px]"
+                onClick={() => setOpenMenu((openMenu) => !openMenu)}
             >
-                <img src="/menu.png" alt="" />
+                <div
+                    className={`h-1 w-6 rounded-sm bg-gray-500 ${openMenu ? 'rotate-45' : ''} origin-left duration-500 ease-in-out`}
+                ></div>
+                <div
+                    className={`h-1 w-6 rounded-sm bg-gray-500 ${openMenu ? 'opacity-0' : ''} duration-500 ease-in-out`}
+                ></div>
+                <div
+                    className={`h-1 w-6 rounded-sm bg-gray-500 ${openMenu ? '-rotate-45' : ''} origin-left duration-500 ease-in-out`}
+                ></div>
             </div>
             <motion.div
                 variants={menuVariants}
@@ -29,12 +39,12 @@ function MobileNav() {
                 animate={openMenu ? 'show' : ''}
                 className="absolute right-0 top-0 z-20 min-h-[40dvh] w-full max-w-xs bg-white text-xl shadow-2xl"
             >
-                <div onClick={() => setOpenMenu(false)}>
+                {/* <div onClick={() => setOpenMenu(false)}>
                     <img
                         src="/cross.png"
                         className="absolute left-4 top-4 z-30 h-6 w-6 cursor-pointer"
                     />
-                </div>
+                </div> */}
 
                 <ul className="absolute top-0 flex h-full w-full flex-col items-center justify-center gap-y-8 md:gap-[4dvw]">
                     <li>
